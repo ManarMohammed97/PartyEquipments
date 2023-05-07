257a3a6a7a62e307e07bf7cb40f33752bac3b581
ManarMohammed97 <manarnasser327@gamil.com>
2023-05-07 13:11:16 +0200

    adding search feld and image to the bar

--- src/components/BorrowingComponent.js
+++ src/components/BorrowingComponent.js
@@ -1,11 +1,13 @@
 import React from "react";
 import BorrowingService from "../services/BorrowingService";
+import logo from "../logo.svg"; // import the logo image
 
 class BorrowingComponent extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       equipments: [],
+      search: "",
     };
   }
 
@@ -15,51 +17,82 @@ class BorrowingComponent extends React.Component {
     });
   }
 
+  updateSearch(event) {
+    this.setState({ search: event.target.value.substr(0, 20) });
+  }
+
   render() {
+    let filteredEquipment = this.state.equipments.filter((borrowing) => {
+      return (
+        borrowing.equipmentName
+          .toLowerCase()
+          .indexOf(this.state.search.toLowerCase()) !== -1
+      );
+    });
+
     return (
       <div>
-        <h1 className="text-center p-3 mb-2 bg-primary text-white">
-          Event-Equipment / Party-Ausstattung
-        </h1>
-        <p></p>
-        <p></p>
+        <nav className="navbar navbar-expand-md navbar-light bg-light navbar navbar-dark bg-dark">
+          <div className="container-sm">
+            <a className="navbar-brand d-flex align-items-center" href="/">
+              <img
+                src={logo}
+                alt="logo"
+                width="30"
+                height="30"
+                className="me-2"
+              />
+              Event-Equipment / Party-Ausstattung
+            </a>
+            <form className="form-inline my-2 my-lg-0">
+              <input
+                className="form-control mr-sm-2"
+                type="search"
+                placeholder="Search"
+                aria-label="Search"
+                value={this.state.search}
+                onChange={this.updateSearch.bind(this)}
+              />
+            </form>
+          </div>
+        </nav>
 
-        <div></div>
-        <table class="table table-striped">
-          <thead class="thead-light">
-            <tr>
-              <th scope="col">
-                <strong>Id</strong>
-              </th>
-              <th scope="col">
-                <strong>Bezeichnung</strong>
-              </th>
-              <th scope="col">
-                <strong>Status</strong>
-              </th>
-            </tr>
-          </thead>
-
-          <tbody>
-            {this.state.equipments.map((borrowing) => (
-              <tr key={borrowing.id}>
-                <td>{borrowing.id}</td>
-                <td>{borrowing.equipmentName}</td>
-                <td
-                  className={
-                    borrowing.status === "AVAILABLE"
-                      ? "text-success"
-                      : borrowing.status === "BORROWED"
-                      ? "text-danger"
-                      : ""
-                  }
-                >
-                  {borrowing.status}
-                </td>{" "}
+        <div className="container-sm my-3">
+          <table className="table table-striped">
+            <thead className="thead-light">
+              <tr>
+                <th scope="col">
+                  <strong>Id</strong>
+                </th>
+                <th scope="col">
+                  <strong>Bezeichnung</strong>
+                </th>
+                <th scope="col">
+                  <strong>Status</strong>
+                </th>
               </tr>
-            ))}
-          </tbody>
-        </table>
+            </thead>
+            <tbody>
+              {filteredEquipment.map((borrowing) => (
+                <tr key={borrowing.id}>
+                  <td>{borrowing.id}</td>
+                  <td>{borrowing.equipmentName}</td>
+                  <td
+                    className={
+                      borrowing.status === "AVAILABLE"
+                        ? "text-success"
+                        : borrowing.status === "BORROWED"
+                        ? "text-danger"
+                        : ""
+                    }
+                  >
+                    {borrowing.status}
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
       </div>
     );
   }
